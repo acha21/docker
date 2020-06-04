426cecc62141616e2718926c080d58b54be2f06e
Yeonchan Ahn <acha21@europa.snu.ac.kr>
2020-06-04 09:54:22 +0900

    remove unnecessaries

--- cuda10.0/entrypoint.sh
+++ cuda10.0/entrypoint.sh
@@ -12,8 +12,6 @@ echo "Starting with UID : $USER_ID"
 
 useradd --shell /bin/bash -u $USER_ID -o -c "" -m $USER_NAME
 
-groupadd $USER_NAME
-
 chown -R $USER_NAME:$USER_NAME /home/$USER_NAME
 
 export HOME='/home/$USER_NAME'
